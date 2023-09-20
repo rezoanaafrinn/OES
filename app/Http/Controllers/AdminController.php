1ea5f79bcf8b984991a4cc11c4047e6e8392e57e
rezoanaafrinn <rezoanaafrinn@gmail.com>
2023-09-20 21:50:59 +0600

    Update,delete

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,7 +7,7 @@ use App\Models\Subject;
 
 class AdminController extends Controller
 {
-    //
+    // Add subject
     public function addSubject(Request $request)
     {
         
@@ -17,6 +17,37 @@ class AdminController extends Controller
             ]);
 
             return response()->json(['success'=>true,'msg'=>'Subject Added Successfully!']); 
+        }catch(\Exception $e){
+            return response()->json(['success'=>false,'msg'=>$e->getMessage()]);
+        };
+         
+    }
+
+    // Edit Subject
+    public function editSubject(Request $request)
+    {
+        
+        try{
+            
+            $subject = Subject::find($request->id);
+            $subject->subject = $request->subject;
+            $subject->save();
+
+            return response()->json(['success'=>true,'msg'=>'Subject Updated Successfully!']); 
+        }catch(\Exception $e){
+            return response()->json(['success'=>false,'msg'=>$e->getMessage()]);
+        };
+         
+    }
+
+    // Delete Subject
+    public function deleteSubject(Request $request)
+    {
+        
+        try{
+            Subject::where('id',$request->id)->delete();
+
+            return response()->json(['success'=>true,'msg'=>'Subject Deleted Successfully!']); 
         }catch(\Exception $e){
             return response()->json(['success'=>false,'msg'=>$e->getMessage()]);
         };
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -24,8 +24,12 @@
         <tr>
           <td> {{ $subject->id }}</td>
           <td> {{ $subject->subject }}</td>
-          <td></td>
-          <td></td>
+          <td>
+            <button class="btn btn-info editButton" data-id="{{ $subject->id }}" data-subject="{{ $subject->subject }}" data-toggle="modal" data-target="#editSubjectModel">Edit</button>
+          </td>
+          <td>
+          <button class="btn btn-danger deleteButton" data-id="{{ $subject->id }}" data-toggle="modal" data-target="#deleteSubjectModel">Delete</button>
+          </td>
         </tr>
       @endforeach
     @else
@@ -61,6 +65,58 @@
   </div>
 </div>
 
+
+<!--Edit Modal -->
+<div class="modal fade" id="editSubjectModel" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">  
+  <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Edit Subject</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form id="editSubject">
+      @csrf
+      <div class="modal-body">
+        <label>Subject: </label>
+        <input type="text" name="subject" placeholder="Re-enter Subject" id="edit_subject" required>
+        <input type="hidden" name="id" id="edit_subject_id">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Update</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<!--Delete Modal -->
+<div class="modal fade" id="deleteSubjectModel" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">  
+  <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Delete Subject</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form id="deleteSubject">
+      @csrf
+      <div class="modal-body">
+        <p>Are you sure you want to delete subject?</p>
+        <input type="hidden" name="id" id="delete_subject_id">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-danger">Delete</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+
 <script>
   $(document).ready(function(){
     $("#addSubject").submit(function(e){
@@ -82,7 +138,66 @@
           }
         }
       });
-    })
+    });
+  
+    // Edit subject
+
+    $(".editButton").click(function(){
+      var subject_id = $(this).attr('data-id');
+      var subject = $(this).attr('data-subject');
+      $("#edit_subject").val(subject);
+      $("#edit_subject_id").val(subject_id);
+    });
+
+    $("#editSubject").submit(function(e){
+      e.preventDefault();
+
+      var formData = $(this).serialize();
+
+      $.ajax({
+        url:"{{ route('editSubject'}}",
+        type:"POST",
+        data:formData,
+        success:function(data){
+          if(data.success == true)
+          {
+            location.reload();
+          }
+          else{
+            alert(data.masg);
+          }
+        }
+      });
+    });
+
+    // Delete subject
+
+    $(".deleteButton").click(function(){
+      var subject_id = $(this).attr('data-id');
+      $("#delete_subject_id").val(subject_id);
+    });
+
+    $("#deleteSubject").submit(function(e){
+      e.preventDefault();
+
+      var formData = $(this).serialize();
+
+      $.ajax({
+        url:"{{ route('deleteSubject'}}",
+        type:"POST",
+        data:formData,
+        success:function(data){
+          if(data.success == true)
+          {
+            location.reload();
+          }
+          else{
+            alert(data.masg);
+          }
+        }
+      });
+    });
+
   });
 </script>
 
